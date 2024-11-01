0b0cbdc61725df47b34a048be9b746c6737da892
Egor <rkrivzha@yandex.ru>
2024-11-01 09:56:38 +0300

    CollisionRegistry refactoring.

--- src/Bomberman - Ton Cats/Assets/Code/Common/Collisions/CollisionRegistry.cs
+++ src/Bomberman - Ton Cats/Assets/Code/Common/Collisions/CollisionRegistry.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections.Generic;
+using Leopotam.EcsLite;
+using Zenject;
 using int_instanceId = System.Int32;
-using int_entity = System.Int32;
 
 namespace Common.Collisions
 {
 	public class CollisionRegistry : ICollisionRegistry
 	{
-		readonly Dictionary<int_instanceId, int_entity> _entityByInstanceId = new();
+		[Inject] EcsWorld _world;
+
+		readonly Dictionary<int_instanceId, EcsPackedEntity> _entityByInstanceId = new();
 
 		public void Register(int instanceId, int entity)
 		{
-			_entityByInstanceId[instanceId] = entity;
+			_entityByInstanceId[instanceId] = _world.PackEntity(entity);
 		}
 
 		public void Unregister(int instanceId)
@@ -19,7 +22,17 @@ namespace Common.Collisions
 				_entityByInstanceId.Remove(instanceId);
 		}
 
-		public bool TryGet(int instanceId, out int entity) =>
-			_entityByInstanceId.TryGetValue(instanceId, out entity);
+		public bool TryGet(int instanceId, out int entity)
+		{
+			entity = default;
+			
+			if (_entityByInstanceId.TryGetValue(instanceId, out var pack) == false)
+				return false;
+
+			if (pack.Unpack(_world, out entity) == false)
+				return false;
+
+			return true;
+		}
 	}
 }

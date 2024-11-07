9d0c61dc99cf3b91ec5cc85da00e52792fc61335
Egor <rkrivzha@yandex.ru>
2024-11-07 12:50:21 +0300

    Hero animator.

---
+++ src/Bomberman - Ton Cats/Assets/Code/Feature/Hero/Behaviour/HeroAnimator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Feature.Hero.Behaviour
+{
+	[RequireComponent(typeof(Animator))]
+	public sealed class HeroAnimator : MonoBehaviour
+	{
+		static readonly int _moveUp = Animator.StringToHash("Move up");
+		static readonly int _moveDown = Animator.StringToHash("Move down");
+		static readonly int _moveLeft = Animator.StringToHash("Move left");
+		static readonly int _moveRight = Animator.StringToHash("Move right");
+		static readonly int _idle = Animator.StringToHash("Idle");
+		static readonly int _death = Animator.StringToHash("Death");
+
+		Animator _animator;
+
+		void Awake()
+		{
+			_animator = GetComponent<Animator>();
+		}
+
+		public void Stop() =>
+			_animator.SetTrigger(_idle);
+
+		public void SetMoveDirection(Vector2 direction)
+		{
+			var x = Mathf.Abs(direction.x);
+			var y = Mathf.Abs(direction.y);
+
+			if (x > y)
+				_animator.SetTrigger(direction.x > 0 ? _moveRight : _moveLeft);
+			else
+				_animator.SetTrigger(direction.y > 0 ? _moveUp : _moveDown);
+		}
+
+		public void Death() =>
+			_animator.SetTrigger(_death);
+	}
+}

f7e4c9bba6a695bbba3310a22d17b29e94d1d4c3
Egor <rkrivzha@yandex.ru>
2024-12-12 14:54:24 +0300

    "EnemyData".

---
+++ src/Bomberman - Ton Cats/Assets/Code/Gameplay/Feature/Enemy/Base/StaticData/EnemyCharacteristic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Gameplay.Feature.Enemy.Base.StaticData
+{
+	[Serializable]
+	public struct EnemyCharacteristic
+	{
+		public float MovementSpeed;
+		public int MovementDistance;
+	}
+}
---
+++ src/Bomberman - Ton Cats/Assets/Code/Gameplay/Feature/Enemy/Base/StaticData/EnemyData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Menu = Constant.CreateAssetMenu.Path;
+
+namespace Gameplay.Feature.Enemy.Base.StaticData
+{
+	[CreateAssetMenu(menuName = Menu.c_StaticData + nameof(EnemyData))]
+	public class EnemyData : ScriptableObject
+	{
+		public string Name;
+		public EnemyCharacteristic Characteristic;
+	}
+}

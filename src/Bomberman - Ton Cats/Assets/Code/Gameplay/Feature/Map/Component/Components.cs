897e1eb9e18cf5447ed34f79ed3ac9afeeab0b6c
Egor <rkrivzha@yandex.ru>
2024-12-18 14:39:51 +0300

    Removes excess code.

--- src/Bomberman - Ton Cats/Assets/Code/Gameplay/Feature/Map/Component/Components.cs
+++ src/Bomberman - Ton Cats/Assets/Code/Gameplay/Feature/Map/Component/Components.cs
@@ -5,8 +5,4 @@ namespace Gameplay.Feature.Map.Component
 	public struct CellPos { public Vector2Int Value; }
 	public struct DestroyedTile { }
 	public struct DestroyedTileRequest { }
-	public struct ChangeMapRequest { }
-	public struct SetFree { }
-	public struct SetDestructible { }
-	public struct SetIndestructible { }
 }

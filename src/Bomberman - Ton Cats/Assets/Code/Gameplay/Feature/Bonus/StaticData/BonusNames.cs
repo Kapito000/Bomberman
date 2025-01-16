2a2ef0b058b409729aa2e23f69cd5eb66eb83cd4
Egor <kapitanov.e@dreamcraftstudio.ru>
2025-01-16 15:39:12 +0300

    Adds the data wrapped into the scriptable object.

--- src/Bomberman - Ton Cats/Assets/Code/Gameplay/Feature/Bonus/StaticData/BonusNames.cs
+++ src/Bomberman - Ton Cats/Assets/Code/Gameplay/Feature/Bonus/StaticData/BonusNames.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
+using Menu = Constant.CreateAssetMenu.Path;
 
 namespace Gameplay.Feature.Bonus.StaticData
 {
-	public sealed class BonusNames : IBonusNames
+	[CreateAssetMenu(menuName = Menu.c_StaticData + nameof(BonusNames))]
+	public sealed class BonusNames : ScriptableObject, IBonusNames
 	{
 		[field: SerializeField] public string Bomb { get; private set; }
 	}

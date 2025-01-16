cee6b19ea875b42b4507a4be568307e056525b06
Egor <kapitanov.e@dreamcraftstudio.ru>
2025-01-16 14:35:14 +0300

    Extends the "SerializedDictionaries" list.

--- src/Bomberman - Ton Cats/Assets/Code/Common/Dictionary/SerializedDictionaries.cs
+++ src/Bomberman - Ton Cats/Assets/Code/Common/Dictionary/SerializedDictionaries.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Common.Dictionary
 {
 	[Serializable]
 	public class StringIntegerDictionary : SerializedDictionary<string, int>
+	{ }
+
+	[Serializable]
+	public class StringSpriteDictionary : SerializedDictionary<string, Sprite>
 	{ }
 }
